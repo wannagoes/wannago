e5d960516c3c8723428dc60a37a29c6354952212
Jorge Fernández <jorgefdezalfonso@gmail.com>
2017-05-13 16:19:59 +0200

    Update jswapo.js

--- jswapo.js
+++ jswapo.js
@@ -1,4 +1,4 @@
-// Modal Image Gallery
+// Modal Image  Gallery
 function onClick(element) {
   document.getElementById("img01").src = element.src;
   document.getElementById("modal01").style.display = "block";
